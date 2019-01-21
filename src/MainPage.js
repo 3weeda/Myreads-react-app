6d40248c7bfebfd1ac0d92784e7b0b9eaa4da560
Ahmad-3weeda <3weeda@gmail.com>
2019-01-22 01:36:15 +0200

    Css updates

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,38 +1,35 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
-import CurrentlyReading from './CurrentlyReading';
-import WantToRead from './WantToRead';
-import Read from './Read';
+import Shelf from './Shelf';
 
 class MainPage extends Component {
+  state={
+    shelves: [
+      {shelf:"Currently Reading", title:"currentlyReading"},
+      {shelf:"Want to Read", title:"wantToRead"},
+      {shelf:"Read", title:"read"}
+    ]
+  }
+
   render() {
     return (
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
-        <div className="list-books-content">
-          <div>
-            {/* All three components are with books prop introduced
-              from App.js Line:35, changeShelf prop introduced
-              from App.js Line 36 */}
-              {/* First shelf component */}
-            <CurrentlyReading
-            books={this.props.books}
-            changeShelf={this.props.changeShelf}
-            />
-            {/* Second shelf component */}
-            <WantToRead
-            books={this.props.books}
-            changeShelf={this.props.changeShelf}
-            />
-            {/* Third shelf component */}
-            <Read
-            books={this.props.books}
-            changeShelf={this.props.changeShelf}
-            />
+        <div className="list-books">
+          <div className="list-books-title">
+            <h1>Library</h1>
           </div>
-        </div>
+          <div className="list-books-content">
+            <div className="library-body">
+              {/* First shelf component */}
+              {this.state.shelves.map(myShelf => {
+                return <Shelf
+                shelf={myShelf.shelf}
+                title={myShelf.title}
+                books={this.props.books}
+                changeShelf={this.props.changeShelf}
+                />
+              })}
+            </div>
+          </div>
         <div className="open-search">
           {/* span to add a title on hover to the Search for a book button
               Link tag with to attribute showing path to the SearchPage*/}
--- src/Shelf.js
+++ src/Shelf.js
@@ -4,22 +4,22 @@ import Book from './Book';
 // I tried a lot to make only one shelf component and
 // iterate over the few minor differences in names
 // between them but couldn't do it, any advice, please?
-class CurrentlyReading extends Component {
+class Shelf extends Component {
   render() {
     return (
       <div className="bookshelf">
-        <h2 className="bookshelf-title">Currently Reading</h2>
+        <h2 className="bookshelf-title"><div className="title-child">{this.props.shelf}</div></h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
             {/* Calling the books array property and filtering it only
             the Currently reading books and drawing the UI of the list items */}
-            {this.props.books.filter(book => book.shelf === 'currentlyReading')
+            {this.props.books.filter(book => book.shelf === this.props.title)
              .map(book => (
                <li key={book.id}>
                  <Book
                  book={book}
                  changeShelf={this.props.changeShelf}
-                 currentShelf="currentlyReading"
+                 currentShelf={this.props.title}
                  />
                </li>))
             }
@@ -29,4 +29,4 @@ class CurrentlyReading extends Component {
 )}
 }
 
-export default CurrentlyReading;
+export default Shelf;
